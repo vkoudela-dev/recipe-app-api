013643765b19055658e31cd61580e1dfc84f11c9
vkoudela-dev <vkoudela@hotmail.com>
2023-05-09 18:26:44 +0100

    Fix linting

--- app/core/models.py
+++ app/core/models.py
@@ -3,7 +3,9 @@ Database models
 """
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
 
 class UserManager(BaseUserManager):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,7 +13,9 @@ class ModelTests(TestCase):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -37,7 +39,9 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser('test@example.com', 'test123')
+        user = get_user_model().objects.create_superuser(
+            'test@example.com', 'test123'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
